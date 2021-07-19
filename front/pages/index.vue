b1337def1a3ffa54c368af24c1d1d8f94c2563e9
HEKUCHAN <Heitorhirose@gmail.com>
2021-07-19 17:49:51 +0900

    AddMyImage

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -3,7 +3,8 @@
     <div id="background" class="background" />
     <div class="background-inner">
       <div class="myinfo-case">
-        <img src="~/assets/icon.png" />
+        <div class="img" />
+        <h1>広瀬 エイトル</h1>
       </div>
     </div>
   </div>
@@ -20,7 +21,7 @@
   height: 100vh;
   width: 100%;
   background: #393e46;
-  z-index: -1;
+  z-index: 1;
 }
 
 .background-inner {
@@ -36,6 +37,45 @@
   .myinfo-case {
     width: 320px;
     height: 550px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+
+    * {
+      z-index: 2;
+      user-select: none;
+    }
+
+    .img {
+      width: 240px !important;
+      height: 240px !important;
+      background-image: url(~static/icon.png);
+      background-size: cover;
+      background-position: center center;
+      border-radius: 50%;
+    }
+
+    h1 {
+      text-align: center;
+      font-weight: bolder;
+      color: #eee;
+      margin-top: 1px;
+    }
+  }
+}
+
+@media screen and (max-width: 580px) {
+  .background-inner {
+    .myinfo-case {
+      width: 280px;
+      height: 550px;
+
+      .img {
+        width: 240px !important;
+        height: 240px !important;
+      }
+    }
   }
 }
 </style>
