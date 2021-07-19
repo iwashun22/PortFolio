248b1b4934fe53e5edee22b20dd06a6777b32116
HEKUCHAN <Heitorhirose@gmail.com>
2021-07-19 21:44:05 +0900

    p

---
+++ front/pages/created/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Created</h1>
+</template>
+
+<script>
+export default {
+  head: {
+    title: '制作物'
+  }
+}
+</script>
+
+<style lang="scss">
+#__layout {
+  margin-top: 58px;
+}
+</style>
---
+++ front/pages/notification/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>notification</h1>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'お知らせ'
+  }
+}
+</script>
+
+<style lang="scss">
+#__layout {
+  margin-top: 58px;
+}
+</style>
