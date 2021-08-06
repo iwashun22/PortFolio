97bc6bb7c4f6aa357f61bd2100026ab3640a778b
HEKUCHAN <Heitorhirose@gmail.com>
2021-08-06 17:14:17 +0900

    'いったんコミット～'

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -6,46 +6,63 @@
         <div class="img" />
         <h2>広瀬 エイトル</h2>
         <div class="icons">
-          <a href="">
-            <img src="~/static/sns/github.svg" alt="">
+          <a href="https://github.com/HEKUCHAN">
+            <img
+              src="~/static/sns/github.svg"
+              alt=""
+              class="img-1"
+            >
           </a>
-          <a href="">
-            <img src="~/static/sns/instagram.png" alt="">
+          <a href="https://www.instagram.com/hirose_heitor/">
+            <img
+              src="~/static/sns/instagram.png"
+              alt=""
+              class="img-2"
+            >
           </a>
-          <a href="">
-            <img src="~/static/sns/mail.svg" alt="">
+          <a href="mailto:Heitorhirose@gmail.com">
+            <img
+              src="~/static/sns/mail.svg"
+              alt=""
+              class="img-3"
+            >
           </a>
-          <a href="">
-            <img src="~/static/sns/twitter.svg" alt="">
+          <a href="https://twitter.com/Heitor_Hirose">
+            <img
+              src="~/static/sns/twitter.svg"
+              alt=""
+              class="img-4"
+            >
           </a>
-          <a href="">
-            <img src="~/static/sns/wantedly.svg" alt="">
+          <a href="https://www.wantedly.com/id/HiroseHeitor">
+            <img
+              src="~/static/sns/wantedly.svg"
+              alt=""
+              class="img-5"
+            >
           </a>
         </div>
       </div>
     </div>
-    <div class="first-main case first-case">
-      <h1 class="title">
-        About Me
-      </h1>
-      <div class="aboutme-info-case">
-        <div class="img" />
-        <div class="msg">
-          <p>テストストストテストストストテストストストテストストストテストストストテストストストテストストストテストストストテストストストテストストストテストストスト</p>
-        </div>
-      </div>
-    </div>
+    <About />
+    <Career />
   </div>
 </template>
 
 <script>
 import bubble from '~/assets/js/particles/bubble.js'
+import About from '~/components/about-me.vue'
+import Career from '~/components/career.vue'
 
 if (process.client) {
   require('particles.js')
 }
 
 export default {
+  components: {
+    About,
+    Career
+  },
   head: {
     titleTemplate: '広瀬エイトル - %s',
     title: 'Main',
@@ -66,57 +83,6 @@ export default {
 </script>
 
 <style lang="scss">
-.first-case {
-  .aboutme-info-case {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-
-    .msg {
-      max-width: 300px;
-
-      p {
-        padding: 14px;
-      }
-    }
-
-    .img {
-      width: 240px !important;
-      height: 240px !important;
-      background-image: url(~static/icon.png);
-      background-size: cover;
-      background-position: center center;
-      border-radius: 50%;
-      margin: 30px 0;
-    }
-  }
-}
-
-.title {
-  border-bottom: solid 3px white;
-  width: fit-content;
-  padding: 6px 20px;
-  margin: 0 30px;
-}
-
-.first-main {
-  width: 100%;
-  height: 600px;
-  background-color: #1e1e1e;
-  margin: calc(100vh + 30px) auto 20px auto;
-  max-width: 980px;
-  border-radius: 0 !important;
-
-  h1 {
-    color: white;
-  }
-}
-
-.case {
-  display: flex;
-  flex-direction: column;
-}
-
 #background {
   position: absolute;
   top: 0;
@@ -188,43 +154,16 @@ export default {
 }
 
 @media screen and (max-width: 580px) {
-  .aboutme-info-case {
-    flex-direction: column !important;
-    align-items: center;
-    max-width: initial;
-
-    .img {
-      max-width: initial !important;
-      margin: 30px auto !important;
-    }
-
-    .msg {
-      width: 100%;
-      max-width: 90vw !important;
-
-      p {
-        width: 100%;
-        max-width: initial !important;
-      }
-    }
-  }
-
   .background-inner {
     .myinfo-case {
       width: 280px;
       height: 550px;
 
       .img {
-        width: 240px !important;
-        height: 240px !important;
+        width: 180px !important;
+        height: 180px !important;
       }
     }
-  }
-
-  .title {
-    font-size: 1.5rem;
-    padding: 3px 10px;
-    margin: 0 15px;
   }
 
   .icons {
